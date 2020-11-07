7c1d67e7e37f226c80d4f4a957ca918281f69af7
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2020-11-07 23:09:36 +0330

    Update CombatTest.cs
    Added Targeting function.

--- Assets/CombatTest.cs
+++ Assets/CombatTest.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPGCharacterAnimsFREE;
 
 public class CombatTest : MonoBehaviour
 {
+    [SerializeField] private RPGCharacterControllerFREE controller = null;
+    
     [SerializeField] private LayerMask enemyMask = 1 << 10;
     [SerializeField] private Vector3 heightOffset = new Vector3();
 
@@ -29,5 +32,43 @@ public class CombatTest : MonoBehaviour
             if (hit.collider.TryGetComponent(out e))
                 e.ReceiveDamage(damage);
         }        
+    }
+    
+    public void LookForTarget()
+    {
+        GameObject targetObj = FindClosestEnemy();
+
+        //If the controller doesnt have a target but theres an enemy nearby
+        //OR
+        //The controller has a target but there ISN'T an enemy nearby
+        if (targetObj != null 
+            || 
+            controller.HasTarget && targetObj == null)
+        {
+            controller.SetTarget(targetObj);
+        }
+    }
+
+    private GameObject FindClosestEnemy()
+    {
+        Collider[] cols = Physics.OverlapSphere(transform.position, attackRadius, enemyMask);
+
+        if (cols.Length == 0)
+            return null;
+
+        GameObject closestEnemyObj = null;
+
+        float close = Mathf.Infinity;
+
+        for (int i = 0; i < cols.Length; i++)
+        {
+            float dist = Vector3.Distance(transform.position, cols[i].transform.position);
+            if (dist < close)
+            {
+                close = dist;
+                closestEnemyObj = cols[i].gameObject;
+            }
+        }
+        return closestEnemyObj;
     }
 }
