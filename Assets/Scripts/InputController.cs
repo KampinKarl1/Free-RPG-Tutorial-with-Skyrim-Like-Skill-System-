24dcf5b5f71abd537ee83e79caa74acee8213fdd
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2020-12-02 12:48:19 +0100

    Add files via upload

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -8,28 +8,44 @@ public class InputController : MonoBehaviour
     [SerializeField] private SkyboxChanger skyboxChanger = null;
     [SerializeField] private SkillsSelectionControl skillsControl = null;
     [SerializeField] private CombatControl combatControl = null;
+    [SerializeField] private RPGCharacterAnimsFREE.RPGCharacterWeaponControllerFREE weapons = null;
     //--------------------------
 
     //--------------------------
-    [SerializeField] KeyCode scrollLeft = KeyCode.A;
-    [SerializeField] KeyCode scrollRight = KeyCode.D;
+    [SerializeField] KeyCode changeSkillPanelStatus = KeyCode.Tab;
+    [SerializeField] KeyCode scrollLeft_Skill = KeyCode.A;
+    [SerializeField] KeyCode scrollRight_Skill = KeyCode.D;
     [SerializeField] private KeyCode skyboxKey = KeyCode.Delete;
+    [SerializeField] private KeyCode swapWeapon = KeyCode.Y;
     //--------------------------
 
     void Update()
     {
-        //Targeting
+        //Targetting
         if (Input.GetAxisRaw("Target") != 0)
             combatControl.LookForTarget();
-            
+
         //Skills
-        if (Input.GetKey(scrollRight) && skillsControl)
+
+        if (Input.GetKey(scrollRight_Skill) && skillsControl && skillsControl.SkillPanelOpen)
             skillsControl.SelectElementRight();
-        if (Input.GetKey(scrollLeft) && skillsControl)
+
+        if (Input.GetKey(scrollLeft_Skill) && skillsControl && skillsControl.SkillPanelOpen)
             skillsControl.SelectElementLeft();
-            
+
+        if (Input.GetKey(changeSkillPanelStatus) && skillsControl && skillsControl.SkillPanelOpen)
+            skillsControl.CloseSkills();
+        if (Input.GetKey(changeSkillPanelStatus) && skillsControl && !skillsControl.SkillPanelOpen)
+            skillsControl.OpenSkills();
+
         //Skybox
         if (Input.GetKeyDown(skyboxKey) && skyboxChanger)
             skyboxChanger.ChangeSkybox();
+
+        //Weapons
+        if (Input.GetKeyDown(swapWeapon) && weapons) 
+        {
+            weapons.SwitchWeaponTwoHand(1);
+        }
     }
 }
